ea43752e2f4083fc4c6185327092a2ce1e4a2c1f
Vladyslav Kopylets <vladyslav.kopylets@gmail.com>
2022-05-29 17:00:47 +0300

    lab 5.0

--- my-new-app/src/Components/Header.js
+++ my-new-app/src/Components/Header.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
     Navbar,
     Nav,
     FormControl,
     Container,
     Form,
-    Button
+    Button,
+    Modal
 } from "react-bootstrap";
 import logo from '../logo.svg';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -15,51 +16,86 @@ import About from '../Pages/About';
 import Blog from '../Pages/Blog';
 
 
-export default class Header extends Component {
-    render() {
-        return (
-            <>
-                <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="/">
-                            <img
-                            src={logo}
-                            height="30"
-                            width="30"
-                            className="d-inline-block align-top"
-                            alt = "Logo"
-                            />
-                            React Site
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="mr-auto">
-                                <Nav.Link href="/"> Home </Nav.Link>
-                                <Nav.Link href="/about"> About us </Nav.Link>
-                                <Nav.Link href="/contacts"> Contacts </Nav.Link>
-                                <Nav.Link href="/blog"> Blog </Nav.Link>
-                            </Nav>
-                            <Form inline="true">
-                                <FormControl
-                                    type ="text"
-                                    placeholder="Search"
-                                    className="mr-sm-2"
-                                />
-                                <Button variant="outline-info">Search</Button>
-                            </Form>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+export default function Header() {
+    const [show, setShow] = useState(false);
 
-                <Router>
-                        <Routes>
-                            <Route path="/" element={<Home />}/>
-                            <Route path="/about" element={<About />}/>
-                            <Route path="/contacts" element={<Contacts />}/>
-                            <Route path="/blog" element={<Blog />}/>
-                        </Routes>
-                </Router>
-            </>
-        )
-    }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                        src={logo}
+                        height="30"
+                        width="30"
+                        className="d-inline-block align-top"
+                        alt = "Logo"
+                        />
+                        React Site
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/"> Home </Nav.Link>
+                            <Nav.Link href="/about"> About us </Nav.Link>
+                            <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                            <Nav.Link href="/blog"> Blog </Nav.Link>
+                        </Nav>
+                        <Form inline="true">
+                            <FormControl
+                                type ="text"
+                                placeholder="Search"
+                                className="mr-sm-2"
+                            />
+                            <Button variant="outline-info">Search</Button>
+                        </Form>
+                    <Button className="ms-2" onClick={handleShow}>Login</Button>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="fromBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter password">
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me"/>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+            <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />}/>
+                        <Route path="/about" element={<About />}/>
+                        <Route path="/contacts" element={<Contacts />}/>
+                        <Route path="/blog" element={<Blog />}/>
+                    </Routes>
+            </Router>
+        </>
+    )
 }
