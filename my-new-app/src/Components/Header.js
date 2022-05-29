5d81eda9f48754485e9e7c198d88e60a666ad38c
Vladyslav Kopylets <vladyslav.kopylets@gmail.com>
2022-05-29 18:30:36 +0300

    lab 5.1

--- my-new-app/src/Components/Header.js
+++ my-new-app/src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     Navbar,
     Nav,
@@ -18,6 +18,56 @@ import Blog from '../Pages/Blog';
 
 export default function Header() {
     const [show, setShow] = useState(false);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailDirty, setEmailDirty] = useState( false)
+    const [passwordDirty, setPasswordDirty] = useState( false)
+    const [emailError, setEmailError] = useState( 'Email не може бути порожнім')
+    const [passwordError, setPasswordError] = useState( 'Пароль не може бути порожнім')
+    const [formValid, setFormValid] = useState(false)
+        //якщо помилка не порожня, то будемо щось робити
+    useEffect ( () => {
+        if (emailError || passwordError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    },[emailError, passwordError])
+
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (!re.test(String(e.target.value.toLowerCase()))) {
+            setEmailError('Некоректний email')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 3 || e.target.length > 8) {
+            setPasswordError('Пароль повинен мати не менше 3 і не більше 8 символів')
+            if (!e.target.value) {
+                setPasswordError('Пароль не може бути порожнім')
+            }
+        } else {
+            setPasswordError('')
+        }
+    }
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -65,7 +115,8 @@ export default function Header() {
                     <Form>
                         <Form.Group controlId="fromBasicEmail">
                             <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            {(emailDirty && emailError) && <div style={{color:"red"}}>{emailError}</div>}
+                            <Form.Control onChange={e => emailHandler(e)} name="email" value={email} onBlur={e => blurHandler(e)} type="email" placeholder="Enter email" />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
@@ -73,20 +124,23 @@ export default function Header() {
 
                         <Form.Group controlId="fromBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter password">
+                            {(passwordError && passwordDirty) && <div style={{color:"red"}}>{passwordError}</div>}
+                            <Form.Control onChange={e => passwordHandler(e)} name="password" value={password} onBlur={e => blurHandler(e)} type="password" placeholder="Enter password">
                             </Form.Control>
                         </Form.Group>
+
 
                         <Form.Group controlId="fromBasicCheckbox">
                             <Form.Check type="checkbox" label="Remember me"/>
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
+                        <Button disabled={!formValid} variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form>
                 </Modal.Body>
             </Modal>
+
 
             <Router>
                     <Routes>
