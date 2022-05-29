ce40bf3084fc7ef58fd4102996553d612fe0b40d
Vladyslav Kopylets <vladyslav.kopylets@gmail.com>
2022-05-29 13:38:37 +0300

    lab 1.0

---
+++ my-new-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <div>
+          <h1> Hello world!</h1>
+          <Button variant="primary">Button</Button>
+      </div>
+  );
+}
+
+
+export default App;
